5244b03583bbaf1ce76b6e0e61109292c942a6b3
lclMetal <lehtinen.lassi.c@gmail.com>
2022-06-19 22:32:24 +0300

    Extract generic data store from texture.c
    Also rename script files for easier addition of new scripts
    between already existing ones.

--- template-project/SABRE-combined.c
+++ template-project/SABRE-combined.c
@@ -161,7 +161,7 @@ void debugMsgFrom(const char *msg, const char *label, int line)
 }
 
 
-// ..\source\global-code\01-vector2.c
+// ..\source\global-code\10-vector2.c
 struct SABRE_Vector2Struct
 {
     float x, y;
@@ -257,7 +257,7 @@ void SABRE_NormalizeVector2InPlace(struct SABRE_Vector2Struct *vec)
 }
 
 
-// ..\source\global-code\02-misc-functions.c
+// ..\source\global-code\20-misc-functions.c
 int SABRE_StringEndsWith(const char *str, const char *str2)
 {
     size_t len1 = strlen(str);
@@ -272,7 +272,7 @@ int SABRE_StringEndsWith(const char *str, const char *str2)
 }
 
 
-// ..\source\global-code\03-engine.c
+// ..\source\global-code\30-engine.c
 enum SABRE_GameStatesEnum
 {
     SABRE_UNINITIALIZED = 0,
@@ -414,7 +414,115 @@ void SABRE_Quit()
 }
 
 
-// ..\source\global-code\04-texture.c
+// ..\source\global-code\35-data-store.c
+#define SABRE_DATA_STORE_DOUBLING_LIMIT 128
+#define SABRE_DATA_STORE_GROW_AMOUNT 64 // SABRE_DATA_STORE_DOUBLING_LIMIT / 2
+
+struct SABRE_DataStoreStruct
+{
+    size_t capacity; // the maximum amount of elements the store can hold at the moment
+    size_t count; // the amount of elements the store actually holds at the moment
+    size_t elemSize; // the size of a single element in the store
+    void *elems; // pointer to the elements
+    void (*addFunc)(struct SABRE_DataStoreStruct*, void*);
+};
+
+void SABRE_SetDataStoreAddFunc(struct SABRE_DataStoreStruct *dataStore, void (*addDataFunc)(struct SABRE_DataStoreStruct*, void*));
+int SABRE_InitDataStore(struct SABRE_DataStoreStruct *dataStore, size_t elemSize);
+int SABRE_GrowDataStore(struct SABRE_DataStoreStruct *dataStore);
+int SABRE_PrepareDataStore(struct SABRE_DataStoreStruct *dataStore);
+int SABRE_AddToDataStore(struct SABRE_DataStoreStruct *dataStore, void *elem);
+void SABRE_FreeDataStore(struct SABRE_DataStoreStruct *dataStore);
+
+void SABRE_SetDataStoreAddFunc(struct SABRE_DataStoreStruct *dataStore, void (*addDataFunc)(struct SABRE_DataStoreStruct*, void*))
+{
+    dataStore->addFunc = addDataFunc;
+}
+
+int SABRE_InitDataStore(struct SABRE_DataStoreStruct *dataStore, size_t elemSize)
+{
+    dataStore->capacity = 16;
+    dataStore->count = 0;
+    dataStore->elemSize = elemSize;
+    dataStore->elems = calloc(dataStore->capacity, dataStore->elemSize);
+
+    if (!dataStore->elems)
+    {
+        DEBUG_MSG_FROM("Memory allocation failed!", "SABRE_InitDataStore");
+        return 1;
+    }
+
+    return 0;
+}
+
+int SABRE_GrowDataStore(struct SABRE_DataStoreStruct *dataStore)
+{
+    void *newElems = NULL;
+
+    // double the data store size or grow it by SABRE_DATA_STORE_GROW_AMOUNT
+    if (dataStore->capacity < SABRE_DATA_STORE_DOUBLING_LIMIT) dataStore->capacity *= 2;
+    else dataStore->capacity += SABRE_DATA_STORE_GROW_AMOUNT;
+
+    newElems = realloc(dataStore->elems, dataStore->capacity);
+
+    if (!newElems)
+    {
+        DEBUG_MSG_FROM("Memory allocation failed!", "SABRE_GrowDataStore");
+        return 1;
+    }
+
+    dataStore->elems = newElems;
+
+    return 0;
+}
+
+int SABRE_PrepareDataStore(struct SABRE_DataStoreStruct *dataStore)
+{
+    // the data store has not been initialized, initialize it and make sure no errors occurred
+    if (!dataStore->capacity && SABRE_InitDataStore(dataStore, dataStore->elemSize) != 0)
+    {
+        DEBUG_MSG_FROM("Unable to initialize data store.", "SABRE_PrepareDataStore");
+        return 1;
+    }
+    // the data store is full, grow it and make sure no errors occurred
+    else if (dataStore->count == dataStore->capacity && SABRE_GrowDataStore(dataStore) != 0)
+    {
+        DEBUG_MSG_FROM("Unable to grow data store.", "SABRE_PrepareDataStore");
+        return 2;
+    }
+    // otherwise no-op
+
+    return 0;
+}
+
+int SABRE_AddToDataStore(struct SABRE_DataStoreStruct *dataStore, void *elem)
+{
+    int err = 0;
+
+    err = SABRE_PrepareDataStore(dataStore);
+
+    if (err != 0) return err;
+
+    dataStore->addFunc(dataStore, elem);
+    dataStore->count++; // new element has been added, increment count
+
+    return 0;
+}
+
+void SABRE_FreeDataStore(struct SABRE_DataStoreStruct *dataStore)
+{
+    if (dataStore->elems)
+    {
+        free(dataStore->elems);
+        dataStore->capacity = 0;
+        dataStore->count = 0;
+        dataStore->elemSize = 0;
+        dataStore->elems = NULL;
+    }
+}
+
+
+// ..\source\global-code\40-texture.c
 #define SABRE_TEXTURE_ACTOR "SABRE_TextureActor"
 
 struct SABRE_TextureStruct
@@ -426,19 +534,9 @@ struct SABRE_TextureStruct
     char name[256];
 };
 
-#define SABRE_DATA_STORE_DOUBLING_LIMIT 128
-#define SABRE_DATA_STORE_GROW_AMOUNT 64 // SABRE_DATA_STORE_DOUBLING_LIMIT / 2
-
 #define SABRE_GET_TEXTURE(DATA_STORE, INDEX) ((struct SABRE_TextureStruct *)(DATA_STORE)->elems)[INDEX]
 
-struct SABRE_DataStoreStruct
-{
-    size_t capacity; // the maximum amount of elements the store can hold at the moment
-    size_t count; // the amount of elements the store actually holds at the moment
-    size_t elemSize; // the size of a single element in the store
-    void *elems; // pointer to the elements
-    void (*addFunc)(struct SABRE_DataStoreStruct*, void*);
-}SABRE_textureStore;
+struct SABRE_DataStoreStruct SABRE_textureStore;
 
 int SABRE_AutoAddTextures();
 int SABRE_AddTexture(const char textureName[256]);
@@ -446,13 +544,6 @@ int SABRE_CalculateTextureWidth(struct SABRE_TextureStruct *texture);
 int SABRE_CalculateTextureHeight(struct SABRE_TextureStruct *texture);
 
 void SABRE_AddTextureToDataStore(struct SABRE_DataStoreStruct *dataStore, void *texture);
-
-void SABRE_SetDataStoreAddFunc(struct SABRE_DataStoreStruct *dataStore, void (*addDataFunc)(struct SABRE_DataStoreStruct*, void*));
-int SABRE_InitDataStore(struct SABRE_DataStoreStruct *dataStore, size_t elemSize);
-int SABRE_GrowDataStore(struct SABRE_DataStoreStruct *dataStore);
-int SABRE_PrepareDataStore(struct SABRE_DataStoreStruct *dataStore);
-int SABRE_AddToDataStore(struct SABRE_DataStoreStruct *dataStore, void *elem);
-void SABRE_FreeDataStore(struct SABRE_DataStoreStruct *dataStore);
 
 // only works for non-animated textures
 int SABRE_AutoAddTextures()
@@ -543,95 +634,8 @@ void SABRE_AddTextureToDataStore(struct SABRE_DataStoreStruct *dataStore, void *
     SABRE_GET_TEXTURE(dataStore, dataStore->count) = (*(struct SABRE_TextureStruct *)texture);
 }
 
-void SABRE_SetDataStoreAddFunc(struct SABRE_DataStoreStruct *dataStore, void (*addDataFunc)(struct SABRE_DataStoreStruct*, void*))
-{
-    dataStore->addFunc = addDataFunc;
-}
 
-int SABRE_InitDataStore(struct SABRE_DataStoreStruct *dataStore, size_t elemSize)
-{
-    dataStore->capacity = 16;
-    dataStore->count = 0;
-    dataStore->elemSize = elemSize;
-    dataStore->elems = calloc(dataStore->capacity, dataStore->elemSize);
-
-    if (!dataStore->elems)
-    {
-        DEBUG_MSG_FROM("Memory allocation failed!", "SABRE_InitDataStore");
-        return 1;
-    }
-
-    return 0;
-}
-
-int SABRE_GrowDataStore(struct SABRE_DataStoreStruct *dataStore)
-{
-    void *newElems = NULL;
-
-    // double the data store size or grow it by SABRE_DATA_STORE_GROW_AMOUNT
-    if (dataStore->capacity < SABRE_DATA_STORE_DOUBLING_LIMIT) dataStore->capacity *= 2;
-    else dataStore->capacity += SABRE_DATA_STORE_GROW_AMOUNT;
-
-    newElems = realloc(dataStore->elems, dataStore->capacity);
-
-    if (!newElems)
-    {
-        DEBUG_MSG_FROM("Memory allocation failed!", "SABRE_GrowDataStore");
-        return 1;
-    }
-
-    dataStore->elems = newElems;
-
-    return 0;
-}
-
-int SABRE_PrepareDataStore(struct SABRE_DataStoreStruct *dataStore)
-{
-    // the data store has not been initialized, initialize it and make sure no errors occurred
-    if (!dataStore->capacity && SABRE_InitDataStore(dataStore, dataStore->elemSize) != 0)
-    {
-        DEBUG_MSG_FROM("Unable to initialize data store.", "SABRE_PrepareDataStore");
-        return 1;
-    }
-    // the data store is full, grow it and make sure no errors occurred
-    else if (dataStore->count == dataStore->capacity && SABRE_GrowDataStore(dataStore) != 0)
-    {
-        DEBUG_MSG_FROM("Unable to grow data store.", "SABRE_PrepareDataStore");
-        return 2;
-    }
-    // otherwise no-op
-
-    return 0;
-}
-
-int SABRE_AddToDataStore(struct SABRE_DataStoreStruct *dataStore, void *elem)
-{
-    int err = 0;
-
-    err = SABRE_PrepareDataStore(dataStore);
-
-    if (err != 0) return err;
-
-    dataStore->addFunc(dataStore, elem);
-    dataStore->count++; // new element has been added, increment count
-
-    return 0;
-}
-
-void SABRE_FreeDataStore(struct SABRE_DataStoreStruct *dataStore)
-{
-    if (dataStore->elems)
-    {
-        free(dataStore->elems);
-        dataStore->capacity = 0;
-        dataStore->count = 0;
-        dataStore->elemSize = 0;
-        dataStore->elems = NULL;
-    }
-}
-
-
-// ..\source\global-code\05-sprite.c
+// ..\source\global-code\50-sprite.c
 #define SABRE_SPRITE_ACTOR "SABRE_SpriteActor"
 
 struct SABRE_SpriteStruct
@@ -659,7 +663,7 @@ struct SABRE_SpriteStruct sprites[SABRE_SPRITE_COUNT] =
 };
 
 
-// ..\source\global-code\06-renderer.c
+// ..\source\global-code\60-renderer.c
 enum SABRE_RenderObjectTypeEnum
 {
     SABRE_TEXTURE_RO,
--- template-project/source/global-code/40-texture.c
+++ template-project/source/global-code/40-texture.c
@@ -9,19 +9,9 @@ struct SABRE_TextureStruct
     char name[256];
 };
 
-#define SABRE_DATA_STORE_DOUBLING_LIMIT 128
-#define SABRE_DATA_STORE_GROW_AMOUNT 64 // SABRE_DATA_STORE_DOUBLING_LIMIT / 2
-
 #define SABRE_GET_TEXTURE(DATA_STORE, INDEX) ((struct SABRE_TextureStruct *)(DATA_STORE)->elems)[INDEX]
 
-struct SABRE_DataStoreStruct
-{
-    size_t capacity; // the maximum amount of elements the store can hold at the moment
-    size_t count; // the amount of elements the store actually holds at the moment
-    size_t elemSize; // the size of a single element in the store
-    void *elems; // pointer to the elements
-    void (*addFunc)(struct SABRE_DataStoreStruct*, void*);
-}SABRE_textureStore;
+struct SABRE_DataStoreStruct SABRE_textureStore;
 
 int SABRE_AutoAddTextures();
 int SABRE_AddTexture(const char textureName[256]);
@@ -29,13 +19,6 @@ int SABRE_CalculateTextureWidth(struct SABRE_TextureStruct *texture);
 int SABRE_CalculateTextureHeight(struct SABRE_TextureStruct *texture);
 
 void SABRE_AddTextureToDataStore(struct SABRE_DataStoreStruct *dataStore, void *texture);
-
-void SABRE_SetDataStoreAddFunc(struct SABRE_DataStoreStruct *dataStore, void (*addDataFunc)(struct SABRE_DataStoreStruct*, void*));
-int SABRE_InitDataStore(struct SABRE_DataStoreStruct *dataStore, size_t elemSize);
-int SABRE_GrowDataStore(struct SABRE_DataStoreStruct *dataStore);
-int SABRE_PrepareDataStore(struct SABRE_DataStoreStruct *dataStore);
-int SABRE_AddToDataStore(struct SABRE_DataStoreStruct *dataStore, void *elem);
-void SABRE_FreeDataStore(struct SABRE_DataStoreStruct *dataStore);
 
 // only works for non-animated textures
 int SABRE_AutoAddTextures()
@@ -124,91 +107,4 @@ int SABRE_CalculateTextureHeight(struct SABRE_TextureStruct *texture)
 void SABRE_AddTextureToDataStore(struct SABRE_DataStoreStruct *dataStore, void *texture)
 {
     SABRE_GET_TEXTURE(dataStore, dataStore->count) = (*(struct SABRE_TextureStruct *)texture);
-}
-
-void SABRE_SetDataStoreAddFunc(struct SABRE_DataStoreStruct *dataStore, void (*addDataFunc)(struct SABRE_DataStoreStruct*, void*))
-{
-    dataStore->addFunc = addDataFunc;
-}
-
-int SABRE_InitDataStore(struct SABRE_DataStoreStruct *dataStore, size_t elemSize)
-{
-    dataStore->capacity = 16;
-    dataStore->count = 0;
-    dataStore->elemSize = elemSize;
-    dataStore->elems = calloc(dataStore->capacity, dataStore->elemSize);
-
-    if (!dataStore->elems)
-    {
-        DEBUG_MSG_FROM("Memory allocation failed!", "SABRE_InitDataStore");
-        return 1;
-    }
-
-    return 0;
-}
-
-int SABRE_GrowDataStore(struct SABRE_DataStoreStruct *dataStore)
-{
-    void *newElems = NULL;
-
-    // double the data store size or grow it by SABRE_DATA_STORE_GROW_AMOUNT
-    if (dataStore->capacity < SABRE_DATA_STORE_DOUBLING_LIMIT) dataStore->capacity *= 2;
-    else dataStore->capacity += SABRE_DATA_STORE_GROW_AMOUNT;
-
-    newElems = realloc(dataStore->elems, dataStore->capacity);
-
-    if (!newElems)
-    {
-        DEBUG_MSG_FROM("Memory allocation failed!", "SABRE_GrowDataStore");
-        return 1;
-    }
-
-    dataStore->elems = newElems;
-
-    return 0;
-}
-
-int SABRE_PrepareDataStore(struct SABRE_DataStoreStruct *dataStore)
-{
-    // the data store has not been initialized, initialize it and make sure no errors occurred
-    if (!dataStore->capacity && SABRE_InitDataStore(dataStore, dataStore->elemSize) != 0)
-    {
-        DEBUG_MSG_FROM("Unable to initialize data store.", "SABRE_PrepareDataStore");
-        return 1;
-    }
-    // the data store is full, grow it and make sure no errors occurred
-    else if (dataStore->count == dataStore->capacity && SABRE_GrowDataStore(dataStore) != 0)
-    {
-        DEBUG_MSG_FROM("Unable to grow data store.", "SABRE_PrepareDataStore");
-        return 2;
-    }
-    // otherwise no-op
-
-    return 0;
-}
-
-int SABRE_AddToDataStore(struct SABRE_DataStoreStruct *dataStore, void *elem)
-{
-    int err = 0;
-
-    err = SABRE_PrepareDataStore(dataStore);
-
-    if (err != 0) return err;
-
-    dataStore->addFunc(dataStore, elem);
-    dataStore->count++; // new element has been added, increment count
-
-    return 0;
-}
-
-void SABRE_FreeDataStore(struct SABRE_DataStoreStruct *dataStore)
-{
-    if (dataStore->elems)
-    {
-        free(dataStore->elems);
-        dataStore->capacity = 0;
-        dataStore->count = 0;
-        dataStore->elemSize = 0;
-        dataStore->elems = NULL;
-    }
 }
