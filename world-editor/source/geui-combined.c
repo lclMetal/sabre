877e9d42bf439901bfb9e42f6c338545fcdde413
lclMetal <lehtinen.lassi.c@gmail.com>
2023-03-08 19:42:42 +0200

    SWE: Update GEUI

--- world-editor/source/geui-combined.c
+++ world-editor/source/geui-combined.c
@@ -1999,6 +1999,14 @@ typedef struct PanelStruct
     struct WindowItemStruct *iList;
 }Panel;
 
+typedef struct DataBindStruct
+{
+    void *data;
+    char tag[256];
+}DataBind;
+
+#define GEUI_MAX_DATA_BINDS_PER_WINDOW 5
+
 typedef struct WindowStruct
 {
     int index;          // window index
@@ -2009,6 +2017,8 @@ typedef struct WindowStruct
     Style style;        // window style
     double zDepth;      // window z depth
     char parentCName[256]; // clonename of the window parent actor
+    int dataBindIndex;  // next available data bind index
+    DataBind dataBinds[GEUI_MAX_DATA_BINDS_PER_WINDOW]; // data binds array
     TileIndices tiles;          // cloneindices of the window tiles
     Panel root;                 // window main panel
     struct WindowStruct *next;  // pointer to next window in list
@@ -2065,6 +2075,86 @@ struct GEUIControllerStruct
 }GEUIController;
 
 #define CURRENT_KEYBOARD GEUIController.kbLayout
+
+
+// ..\source\geui\11-geui-data-binds.c
+void initWindowDataBinds(Window *window)
+{
+    int i;
+
+    if (!window) return;
+
+    for (i = 0; i < GEUI_MAX_DATA_BINDS_PER_WINDOW; i++)
+    {
+        window->dataBinds[i].data = NULL;
+        strcpy(window->dataBinds[i].tag, "");
+    }
+}
+
+int addDataBind(Window *window, char tag[256], void *data)
+{
+    if (!window) return 1; // window is NULL
+    if (window->dataBindIndex >= GEUI_MAX_DATA_BINDS_PER_WINDOW)
+        return 2; // max data binds already in use
+
+    window->dataBinds[window->dataBindIndex].data = data;
+    strcpy(window->dataBinds[window->dataBindIndex].tag, tag);
+    window->dataBindIndex++;
+
+    return 0;
+}
+
+int updateDataBind(Window *window, char tag[256], void *data)
+{
+    int i;
+
+    if (!window) return 1; // window is NULL
+
+    for (i = 0; i < GEUI_MAX_DATA_BINDS_PER_WINDOW; i++)
+    {
+        if (!strcmp(window->dataBinds[i].tag, tag))
+        {
+            window->dataBinds[i].data = data;
+            return 0;
+        }
+    }
+
+    return 2; // data bind not found
+}
+
+void removeDataBind(Window *window, char tag[256])
+{
+    int i;
+
+    if (!window) return; // window is NULL
+
+    for (i = 0; i < GEUI_MAX_DATA_BINDS_PER_WINDOW; i++)
+    {
+        if (!strcmp(window->dataBinds[i].tag, tag))
+        {
+            window->dataBinds[i].data = NULL;
+            strcpy(window->dataBinds[i].tag, "");
+            return;
+        }
+    }
+}
+
+void *getBoundData(Window *window, char tag[256])
+{
+    int i;
+
+    if (!window) return NULL; // window is NULL
+
+    for (i = 0; i < GEUI_MAX_DATA_BINDS_PER_WINDOW; i++)
+    {
+        if (!strcmp(window->dataBinds[i].tag, tag))
+        {
+            return window->dataBinds[i].data;
+        }
+    }
+
+    return NULL;
+}
 
 
 // ..\source\geui\12-geui-screen-coords.c
