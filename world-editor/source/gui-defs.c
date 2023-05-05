bf406badad130f2e24466c7f457d97c947cb3d2f
lclMetal <lehtinen.lassi.c@gmail.com>
2023-05-05 21:41:44 +0300

    Update gui-defs.c

--- world-editor/source/gui-defs.c
+++ world-editor/source/gui-defs.c
@@ -1,58 +1,180 @@
 #define SWE_WINDOW_WORLD_CONTEXT_MENU   "worldContextMenu"
 #define SWE_WINDOW_NEW_PROJECT_ALERT    "newProjectAlert"
+#define SWE_WINDOW_OPEN_PROJECT         "openProjectWindow"
+#define SWE_WINDOW_SAVE_PROJECT         "saveProjectWindow"
 #define SWE_WINDOW_PLACE_ENTITY         "placeEntityWindow"
 #define SWE_WINDOW_CREATE_ENTITY_TYPE   "createEntityTypeWindow"
 #define SWE_WINDOW_PLACE_TRIGGER        "placeTriggerWindow"
 #define SWE_WINDOW_TXR_CONFIG           "configureTexture"
+#define SWE_WINDOW_LEVEL_PROPERTIES     "levelProperties"
 
-void SWE_CreateWindows()
+Style sweStyle;
+Style contextMenuStyle;
+
+Font titleFontYes =
+{
+     1, // Letter spacing in pixels
+     6, // Word spacing in pixels
+    25, // Line spacing in pixels
+     4, // Baseline offset in pixels
+    28, // Indentation in pixels
+    "segoeui20", // Font animation name
+    {   // Character widths
+         4,  6, 12,  9, 16, 15,  2,  5,  5,  8,
+        10,  3,  6,  4,  9, 10,  6, 10,  9, 11,
+         9, 10, 10, 10, 10,  4,  4, 10, 10, 10,
+         8, 16, 14,  9, 12, 13,  9,  9, 12, 12,
+         2,  6, 10,  8, 16, 12, 14,  9, 15, 11,
+        10, 10, 12, 13, 20, 12, 12, 12,  5,  8,
+         5, 10, 10,  4,  9, 10,  8, 10, 10,  7,
+        10, 10,  3,  7,  9,  2, 15, 10, 10, 10,
+        10,  6,  8,  6, 10, 10, 14, 10, 10, 10,
+         6,  2,  6, 10
+    }
+};
+
+Font labelFontYes =
+{
+     1, // Letter spacing in pixels
+     4, // Word spacing in pixels
+    19, // Line spacing in pixels
+     3, // Baseline offset in pixels
+    21, // Indentation in pixels
+    "segoeui16", // Font animation name
+    {   // Character widths
+         2,  4, 10,  8, 12, 13,  2,  5,  5,  6,
+         8,  3,  6,  2,  8,  8,  5,  8,  8,  9,
+         8,  8,  8,  8,  8,  2,  3,  8,  8,  8,
+         6, 14, 10,  8, 10, 10,  7,  7, 10, 10,
+         2,  5,  9,  7, 12, 10, 12,  8, 13,  9,
+         8,  8, 10, 10, 16, 10, 10, 10,  4,  8,
+         4,  8,  8,  4,  7,  8,  7,  8,  8,  6,
+         8,  8,  2,  5,  7,  2, 12,  8,  8,  8,
+         8,  5,  6,  6,  8,  8, 12,  8,  8,  8,
+         4,  2,  4,  8
+    }
+};
+
+Font textFontYes =
+{
+     1, // Letter spacing in pixels
+     4, // Word spacing in pixels
+    19, // Line spacing in pixels
+     3, // Baseline offset in pixels
+    21, // Indentation in pixels
+    "segoeui16", // Font animation name
+    {   // Character widths
+         2,  4, 10,  8, 12, 13,  2,  5,  5,  6,
+         8,  3,  6,  2,  8,  8,  5,  8,  8,  9,
+         8,  8,  8,  8,  8,  2,  3,  8,  8,  8,
+         6, 14, 10,  8, 10, 10,  7,  7, 10, 10,
+         2,  5,  9,  7, 12, 10, 12,  8, 13,  9,
+         8,  8, 10, 10, 16, 10, 10, 10,  4,  8,
+         4,  8,  8,  4,  7,  8,  7,  8,  8,  6,
+         8,  8,  2,  5,  7,  2, 12,  8,  8,  8,
+         8,  5,  6,  6,  8,  8, 12,  8,  8,  8,
+         4,  2,  4,  8
+    }
+};
+
+void applyTextureConfig(GUIAction *action)
+{
+    SABRE_Texture *txr = getBoundData(action->window, "texture");
+
+    if (txr)
+    {
+        txr->isWindow = getCheckboxState(getItemByTag(action->window, "chkAttribIsWindow"), False);
+    }
+
+    closeWindow(action->window);
+}
+
+void saveProject(GUIAction *action)
+{
+    // saveProjectData(addFileExtension(readTextInput(getItemByTag(action->window, "fileName")), "swp"));
+    writeCurrentLevelProjectData(addFileExtension(readTextInput(getItemByTag(action->window, "fileName")), "swp"));
+    closeWindow(action->window);
+}
+
+void loadProject(GUIAction *action)
+{
+    loadProjectData(addFileExtension(readTextInput(getItemByTag(action->window, "fileName")), "swp"));
+    closeWindow(action->window);
+}
+
+void populateTextureConfigWindow(SABRE_Texture *txr)
+{
+    char temp[256];
+    Window *window = getWindowByTag(SWE_WINDOW_TXR_CONFIG);
+    WindowItem *item = NULL;
+
+    if ((item = getItemByTag(window, "title")) && item->type == GEUI_Text)
+    {
+        sprintf(temp, "\nTexture \"%s\" configuration", txr->name);
+        setTextContent(item, temp);
+    }
+
+    if ((item = getItemByTag(window, "chkAttribIsWindow")) && item->type == GEUI_Checkbox)
+    {
+        setCheckboxState(item, txr->isWindow);
+    }
+
+    updateDataBind(window, "texture", txr);
+    openWindow(SWE_WINDOW_TXR_CONFIG, GEUI_WPOS_SCREEN_CENTER);
+}
+
+void SWE_CreateWindows(Style *ctxMenuStyle, Style *sweMainStyle)
 {
     Window  *window;
     Panel   *panel;
 
-    Style contextMenuStyle =
-        createStyle("gui_sheet_small",
-            &defTitleFont, &defLabelFont, &segoeui12,
-            0, 2, GEUI_BUTTON_TEXT_ALIGN_LEFT | GEUI_BUTTON_STRETCH, 0.1f,
-            WHITE, WHITE, WHITE, BLACK, BLACK, BLACK, WHITE,
-            createRGB(210, 255, 255, 1.0), createRGB(150, 255, 255, 1.0), createRGB(128, 128, 128, 1.0));
-
-    Style sweStyle =
-        createStyle("gui_sheet_default",
-            &defTitleFont, &defLabelFont, &defTextFont,
-            5, 2, 0, 1.0f, WHITE, WHITE, WHITE, BLACK, BLACK, BLACK, createRGB(235, 235, 235, 1.0),
-            createRGB(210, 255, 255, 1.0), createRGB(150, 255, 255, 1.0), createRGB(64, 128, 255, 1.0));
-
     // Define world context menu
-    window = createWindow(SWE_WINDOW_WORLD_CONTEXT_MENU, contextMenuStyle);
-    getTileDimensions(&window->style);
+    window = createWindow(SWE_WINDOW_WORLD_CONTEXT_MENU, GEUI_NO_TITLE, ctxMenuStyle);
+    getTileDimensions(window->style);
     panel = getWindowRootPanel(window);
     setPosition(addButton(panel, "placeEntityBtn", "Place entity", createOpenWindowAction(SWE_WINDOW_PLACE_ENTITY, GEUI_WPOS_MOUSE_TOP)), 0, 0);
     setPosition(addButton(panel, "placeTriggerBtn", "Place trigger     ", createOpenWindowAction(SWE_WINDOW_PLACE_TRIGGER, GEUI_WPOS_MOUSE_TOP)), 1, 0);
     // setPosition(addButton(panel, "closeBtn", "Close", createCloseWindowAction(window->tag)), 2, 0);
 
     // Define "new project" alert window
-    window = createWindow(SWE_WINDOW_NEW_PROJECT_ALERT, sweStyle);
+    window = createWindow(SWE_WINDOW_NEW_PROJECT_ALERT, "Create new project?", sweMainStyle);
     panel = getWindowRootPanel(window);
-    setPosition(addText(panel, "title", "\nCreate new project?", 0), 0, 0);
     setPosition(addText(panel, "alertText",
-        "Creating a new project will discard the current one. Continue?", 300), 1, 0);
-    panel = getPanel(setPosition(addPanel(panel, "buttonsPanel"), 2, 0));
+        "Creating a new project will discard the current one. Continue?", 300), 0, 0);
+    panel = getPanel(setPosition(addPanel(panel, "buttonsPanel"), 1, 0));
     setPosition(addButton(panel, "okBtn", "Ok", createCloseWindowAction(window->tag)), 0, 0);
     setPosition(addButton(panel, "cancelBtn", "Cancel", createCloseWindowAction(window->tag)), 0, 1);
 
-    // Define "place entity" window
-    window = createWindow(SWE_WINDOW_PLACE_ENTITY, sweStyle);
+    // Define "open project" window
+    window = createWindow(SWE_WINDOW_OPEN_PROJECT, "Open project", sweMainStyle);
     panel = getWindowRootPanel(window);
-    setPosition(addText(panel, "title", "\nPlace entity", 0), 0, 0);
+    setPosition(addText(panel, "title", "Project file name", 0), 0, 0);
+    setPosition(addInputField(panel, "fileName", "untitled", createTextInputSettings(), 200), 1, 0);
+    panel = getPanel(setPosition(addPanel(panel, "buttonsPanel"), 2, 0));
+    setPosition(addButton(panel, "openBtn", "Open", createAction(loadProject)), 0, 0);
+    setPosition(addButton(panel, "cancelBtn", "Cancel", createCloseWindowAction(window->tag)), 0, 1);
+
+    // Define "save project" window
+    window = createWindow(SWE_WINDOW_SAVE_PROJECT, "Save project", sweMainStyle);
+    panel = getWindowRootPanel(window);
+    setPosition(addText(panel, "title", "Project file name", 0), 0, 0);
+    setPosition(addInputField(panel, "fileName", "untitled", createTextInputSettings(), 200), 1, 0);
+    panel = getPanel(setPosition(addPanel(panel, "buttonsPanel"), 2, 0));
+    setPosition(addButton(panel, "saveBtn", "Save", createAction(saveProject)), 0, 0);
+    setPosition(addButton(panel, "cancelBtn", "Cancel", createCloseWindowAction(window->tag)), 0, 1);
+
+    // Define "place entity" window
+    window = createWindow(SWE_WINDOW_PLACE_ENTITY, "Place entity", sweMainStyle);
+    panel = getWindowRootPanel(window);
+    setPosition(addText(panel, "title", "Place entity", 0), 0, 0);
     panel = getPanel(setPosition(addPanel(panel, "buttonsPanel"), 1, 0));
     setPosition(addButton(panel, "okBtn", "Ok", createCloseWindowAction(window->tag)), 0, 0);
     setPosition(addButton(panel, "createNewBtn", "Create new entity type", createOpenWindowAction(SWE_WINDOW_CREATE_ENTITY_TYPE, GEUI_WPOS_SCREEN_CENTER)), 0, 1);
 
     // Define "create entity type" window
-    window = createWindow(SWE_WINDOW_CREATE_ENTITY_TYPE, sweStyle);
+    window = createWindow(SWE_WINDOW_CREATE_ENTITY_TYPE, "Create new entity type", sweMainStyle);
     panel = getWindowRootPanel(window);
-    setPosition(addText(panel, "title", "\nCreate new entity type\n", 0), 0, 0);
+    setPosition(addText(panel, "title", "Entity settings\n", 0), 0, 0);
     panel = getPanel(setPosition(addPanel(panel, "propertiesPanel"), 1, 0));
     setPosition(addText(panel, "labelName", "Name", 200), 0, 0);
     setPosition(addInputField(panel, "inputName", "Name", createTextInputSettings(), 200), 0, 1);
@@ -69,15 +191,16 @@ void SWE_CreateWindows()
     setPosition(addButton(panel, "cancelBtn", "Cancel", createCloseWindowAction(window->tag)), 0, 1);
 
     // Define "place trigger" window
-    window = createWindow(SWE_WINDOW_PLACE_TRIGGER, sweStyle);
+    window = createWindow(SWE_WINDOW_PLACE_TRIGGER, "Place trigger", sweMainStyle);
     panel = getWindowRootPanel(window);
-    setPosition(addText(panel, "title", "\nPlace trigger", 0), 0, 0);
+    setPosition(addText(panel, "title", "Place trigger", 0), 0, 0);
     setPosition(addButton(panel, "okBtn", "Ok", createCloseWindowAction(window->tag)), 1, 0);
 
     // Define "texture config" window
-    window = createWindow(SWE_WINDOW_TXR_CONFIG, sweStyle);
+    window = createWindow(SWE_WINDOW_TXR_CONFIG, "Configure texture", sweMainStyle);
     panel = getWindowRootPanel(window);
-    setPosition(addText(panel, "title", "\nTexture config:", 0), 0, 0);
+    addDataBind(window, "texture", NULL);
+    setPosition(addText(panel, "title", "Texture settings:", 0), 0, 0);
     // panel = getPanel(setPosition(addPanel(panel, "propertiesPanel"), 1, 0));
     // setPosition(addText(panel, "labelName", "Name", 200), 0, 0);
     // setPosition(addText(panel, "valueName", "Name", 200), 0, 1);
@@ -86,6 +209,14 @@ void SWE_CreateWindows()
     setPosition(addCheckbox(panel, "chkAttribIsWindow", False), 0, 0);
     setPosition(addText(panel, "labelAttribIsWindow", "Is window", 200), 0, 1);
     panel = getPanel(setPosition(addPanel(getWindowRootPanel(window), "buttonsPanel"), 3, 0));
-    setPosition(addButton(panel, "doneBtn", "Done", createCloseWindowAction(window->tag)), 0, 0);
+    setPosition(addButton(panel, "doneBtn", "Done", createAction(applyTextureConfig)), 0, 0);
     setPosition(addButton(panel, "cancelBtn", "Cancel", createCloseWindowAction(window->tag)), 0, 1);
+
+    // Define the "level properties" window
+    window = createWindow(SWE_WINDOW_LEVEL_PROPERTIES, "Edit level properties", sweMainStyle);
+    panel = getWindowRootPanel(window);
+    setPosition(addText(panel, "title", "Level properties:", 0), 0, 0);
+    setPosition(addText(panel, "labelColors", "Colors", 200), 1, 0);
+    panel = getPanel(setPosition(addPanel(getWindowRootPanel(window), "colorsPanel"), 2, 0));
+    //setPosition(addInputField(panel, ""))
 }
